b5ce27ff31037a850ad883ecac7641b38d533c9b
aaansari123 <asadali1029@gmail.com>
2022-10-11 13:59:39 -0400

    fixed up contact page a little

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../assets/style.css'
 export default function Contact() {
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [message,setMessage] = useState('');
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    setName('');
+    setEmail('');
+    setMessage('');
+    
+    
+  };
   return (
     <div>
       <h1>Contact Page</h1>
-      <form >
+      <form onSubmit = {handleFormSubmit}>
         <input
+          id = "name"
+          name="name"
           type="text"
           placeholder="Enter your name"
-          name="text"
-
-
-        ></input>
+          onChange = {event => setName(event.target.value)}
+          value = {name}
+        />
          <input
+          id = "email"
           type="text"
           placeholder="Enter your email"
-          name="text"
+          name="email"
+          onChange = {event => setEmail(event.target.value)}
+          value = {email}
 
 
-        ></input>
+        />
          <input
+          id = "message"
           type="text"
           placeholder="Enter a message"
-          name="text"
+          name="message"
+          onChange = {event => setMessage(event.target.value)}
+          value = {message}
 
 
-        ></input>
+        />
+        <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        
     </div>
   );
 }
